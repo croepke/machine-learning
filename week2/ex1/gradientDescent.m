4c84fed83ffb6cdc8f52b921c5b643f2fb566b4d
Christian Röpke <croepke@posteo.de>
2018-07-09 07:11:16 +0200

    Add univariate linear regression

--- week2/ex1/gradientDescent.m
+++ week2/ex1/gradientDescent.m
@@ -16,13 +16,9 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
-
-
-
-
+    theta_old = theta;
+    for t = 1:length(theta_old)
+        theta(t) = theta(t) - (alpha/m) * sum(((X*theta_old)-y).*X(:,t));
     % ============================================================
 
     % Save the cost J in every iteration    
