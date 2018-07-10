429101b36bb87a5a02298ab0ad3148ac49df7793
Christian Röpke <croepke@posteo.de>
2018-07-10 07:47:18 +0200

    Working multivariate linear regression and normal equation solutions

--- week2/ex1/ex1_multi.m
+++ week2/ex1/ex1_multi.m
@@ -1,3 +1,4 @@
+
 %% Machine Learning Online Class
 %  Exercise 1: Linear regression with multiple variables
 %
@@ -89,9 +90,22 @@ num_iters = 400;
 theta = zeros(3, 1);
 [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters);
 
+% Init Theta and Run Gradient Descent (alpha=0.03)
+theta2 = zeros(3, 1)
+alpha2 = 0.03;
+[theta2, J_history2] = gradientDescentMulti(X, y, theta2, alpha2, num_iters);
+
+% Init Theta and Run Gradient Descent (alpha=0.1)
+theta3 = zeros(3, 1)
+alpha3 = 0.1;
+[theta3, J_history3] = gradientDescentMulti(X, y, theta3, alpha3, num_iters);
+
 % Plot the convergence graph
 figure;
 plot(1:numel(J_history), J_history, '-b', 'LineWidth', 2);
+hold on;
+plot(1:numel(J_history2), J_history2, '-r', 'LineWidth', 2);
+plot(1:numel(J_history3), J_history3, '-k', 'LineWidth', 2);
 xlabel('Number of iterations');
 ylabel('Cost J');
 
@@ -104,7 +118,10 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
+house = [1650 3];
+house_norm = (house.-mu)./sigma;
+house_norm = [1 house_norm];
+price = house_norm*theta; % You should change this
 
 
 % ============================================================
@@ -149,7 +166,7 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
+price = [1 house]*theta; % You should change this
 
 
 % ============================================================
--- week2/ex1/gradientDescentMulti.m
+++ week2/ex1/gradientDescentMulti.m
@@ -16,16 +16,9 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
-
-
-
-
-
-
-
-
-
-
+    theta_old = theta;
+    for i = 1:size(X,2)
+        theta(i) = theta(i) - (alpha/m)* sum(((X*theta_old)-y).*X(:,i));
 
     % ============================================================
 
